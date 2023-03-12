e3dbf25eb1af03a3e8c3c46ef26bf34f0067ce90
Nick Young <yungnickyoung@gmail.com>
2023-03-11 21:23:42 -0500

    First floor rooms

--- Common/src/main/java/com/yungnickyoung/minecraft/betterjungletemples/module/StructureProcessorTypeModule.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/betterjungletemples/module/StructureProcessorTypeModule.java
@@ -3,6 +3,7 @@ package com.yungnickyoung.minecraft.betterjungletemples.module;
 import com.yungnickyoung.minecraft.betterjungletemples.BetterJungleTemplesCommon;
 import com.yungnickyoung.minecraft.betterjungletemples.world.processor.BlockReplaceProcessor;
 import com.yungnickyoung.minecraft.betterjungletemples.world.processor.EmptyDispenserProcessor;
+import com.yungnickyoung.minecraft.betterjungletemples.world.processor.FireballDispenserProcessor;
 import com.yungnickyoung.minecraft.betterjungletemples.world.processor.LiquidBlockProcessor;
 import com.yungnickyoung.minecraft.betterjungletemples.world.processor.PillarProcessor;
 import com.yungnickyoung.minecraft.betterjungletemples.world.processor.WaterlogProcessor;
@@ -25,4 +26,7 @@ public class StructureProcessorTypeModule {
 
     @AutoRegister("empty_dispenser_processor")
     public static StructureProcessorType<EmptyDispenserProcessor> EMPTY_DISPENSER_PROCESSOR = () -> EmptyDispenserProcessor.CODEC;
+
+    @AutoRegister("fireball_dispenser_processor")
+    public static StructureProcessorType<FireballDispenserProcessor> FIREBALL_DISPENSER_PROCESSOR = () -> FireballDispenserProcessor.CODEC;
 }
---
+++ Common/src/main/java/com/yungnickyoung/minecraft/betterjungletemples/world/processor/FireballDispenserProcessor.java
@@ -0,0 +1,59 @@
+package com.yungnickyoung.minecraft.betterjungletemples.world.processor;
+
+import com.mojang.serialization.Codec;
+import com.yungnickyoung.minecraft.betterjungletemples.module.StructureProcessorTypeModule;
+import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DispenserBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessor;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorType;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+public class FireballDispenserProcessor extends StructureProcessor {
+    public static final FireballDispenserProcessor INSTANCE = new FireballDispenserProcessor();
+    public static final Codec<FireballDispenserProcessor> CODEC = Codec.unit(() -> INSTANCE);
+
+    @Override
+    public StructureTemplate.StructureBlockInfo processBlock(LevelReader levelReader,
+                                                             BlockPos jigsawPiecePos,
+                                                             BlockPos jigsawPieceBottomCenterPos,
+                                                             StructureTemplate.StructureBlockInfo blockInfoLocal,
+                                                             StructureTemplate.StructureBlockInfo blockInfoGlobal,
+                                                             StructurePlaceSettings structurePlacementData) {
+        if (blockInfoGlobal.state.is(Blocks.ORANGE_CONCRETE)) {
+            BlockState blockState = Blocks.DISPENSER.defaultBlockState().setValue(DispenserBlock.FACING, Direction.UP);
+            CompoundTag newNbt = new CompoundTag();
+            ListTag items = new ListTag();
+            RandomSource randomSource = structurePlacementData.getRandom(blockInfoGlobal.pos);
+
+            for (int slot = 0; slot < 9; slot++) {
+                if (randomSource.nextFloat() < 0.1f || slot == 4) {
+                    CompoundTag slotTag = new CompoundTag();
+                    slotTag.putByte("Slot", (byte) slot);
+                    slotTag.putString("id", "minecraft:fire_charge");
+                    slotTag.putByte("Count", (byte) 1);
+                    items.add(slotTag);
+                }
+            }
+            newNbt.put("Items", items);
+            blockInfoGlobal = new StructureTemplate.StructureBlockInfo(blockInfoGlobal.pos, blockState, newNbt);
+        }
+        return blockInfoGlobal;
+    }
+
+    protected StructureProcessorType<?> getType() {
+        return StructureProcessorTypeModule.FIREBALL_DISPENSER_PROCESSOR;
+    }
+}
