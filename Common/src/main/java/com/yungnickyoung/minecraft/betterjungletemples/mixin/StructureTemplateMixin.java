91e001a098dcc8c10b7af049be2b8022de4ca56e
Nick Young <yungnickyoung@gmail.com>
2023-06-18 01:33:11 -0400

    Update water parkour floor2 rooms and add new staircase w/ chest.

--- Common/src/main/java/com/yungnickyoung/minecraft/betterjungletemples/mixin/StructureTemplateMixin.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/betterjungletemples/mixin/StructureTemplateMixin.java
@@ -12,15 +12,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-/**
- * Makes it so a block's waterlogged state is not based solely on the presence of water at the block's position.
- *
- * @author TelepathicGrunt
- */
 @Mixin(StructureTemplate.class)
 public class StructureTemplateMixin {
-    @Inject(method = "placeInWorld",
-            at = @At(value = "HEAD"))
+    /**
+     * Makes it so a block's waterlogged state is not based solely on the presence of water at the block's position.
+     *
+     * @author TelepathicGrunt
+     */
+    @Inject(method = "placeInWorld", at = @At(value = "HEAD"))
     private void betterjungletemples_preventAutoWaterlogging(ServerLevelAccessor serverLevelAccessor, BlockPos blockPos1, BlockPos blockPos2, StructurePlaceSettings structurePlaceSettings, RandomSource random, int flag, CallbackInfoReturnable<Boolean> cir) {
         if (structurePlaceSettings.getProcessors()
                 .stream()
